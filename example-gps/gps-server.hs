a2a1da00d9de99932e9619800f8e9e1eccb8f906
Dennis <what@what.what>
2013-05-06 01:04:36 +0900

    Get random points on map.

--- example-gps/gps-server.hs
+++ example-gps/gps-server.hs
@@ -6,6 +6,7 @@ import Control.Exception (fromException)
 import Control.Monad (forM_, forever)
 import Control.Concurrent (MVar, newMVar, modifyMVar_, readMVar, forkIO, threadDelay)
 import Control.Monad.IO.Class (liftIO)
+import System.Random (StdGen, randomRIO, newStdGen, getStdGen, randoms, random, randomR)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Network.WebSockets as WS
@@ -34,6 +35,16 @@ broadcast message clients = do
     T.putStrLn message
     forM_ clients $ \(_, sink) -> WS.sendSink sink $ WS.textData message
 
+randLat :: StdGen -> (Double, StdGen)
+randLat gen =
+    let (double, newgen) = randomR (35.665304, 35.724972) gen :: (Double, StdGen)
+    in (double, newgen)
+
+randLon :: StdGen -> (Double, StdGen)
+randLon gen =
+    let (double, newgen) = randomR (139.694939, 139.787292) gen :: (Double, StdGen)
+    in (double, newgen)
+
 main :: IO ()
 main = do
     state <- newMVar newServerState
@@ -49,6 +60,12 @@ spam :: WS.TextProtocol p => WS.WebSockets p ()
 spam = do
     liftIO $ putStrLn "Connected"
     forever $ do
-        liftIO $ threadDelay $ 5000000
-        liftIO $ putStrLn "Sending 35.694726 139.742832"
-        WS.sendTextData ("35.694726 139.742832" :: Text)
+        gen <- liftIO $ newStdGen
+        let (lat, newgen) = randLat gen
+        let latstr = show lat
+        let (lon, newgen') = randLon newgen
+        let lonstr = show lon
+        liftIO $ threadDelay $ 3000000
+        liftIO $ putStrLn ("Sending " `mappend` latstr `mappend` " "
+            `mappend` lonstr)
+        WS.sendTextData ((T.pack (latstr `mappend` " " `mappend` lonstr)) :: Text)
