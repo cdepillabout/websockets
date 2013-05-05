0e374d3b5b5f08068169f2c697f027d54c1cb666
Dennis <what@what.what>
2013-05-05 21:31:48 +0900

    Make server that sends coordinates once a second.

--- example-gps/gps-client.hs
+++ example-gps/gps-client.hs
@@ -10,22 +10,12 @@ import qualified Network.WebSockets as WS
 app :: WS.WebSockets WS.Hybi10 ()
 app = do
     liftIO $ print "Connected"
-
-    -- Fork off a separate thread that reads from stdin and writes to the sink.
-    sink <- WS.getSink
-    liftIO $ forkIO $ readInput sink
-
     forever loop
   where
     loop = do
         msg <- WS.receiveData
         liftIO $ T.putStrLn msg
 
-readInput :: (WS.TextProtocol p) => WS.Sink p -> IO ()
-readInput sink = forever $ do
-    line <- T.getLine
-    WS.sendSink sink $ WS.textData line
-
 main :: IO ()
 main = do
-    WS.connect "127.0.0.1" 9160 "/chat" app
+    WS.connect "127.0.0.1" 9160 "/" app
--- example-gps/gps-client.js
+++ example-gps/gps-client.js
@@ -9,31 +9,12 @@ function createWebSocket(path) {
 
 var users = [];
 
-function refreshUsers() {
-    $('#users').html('');
-    for(i in users) {
-        $('#users').append($(document.createElement('li')).text(users[i]));
-    }
-}
 
 function onMessage(event) {
     var p = $(document.createElement('p')).text(event.data);
 
     $('#messages').append(p);
     $('#messages').animate({scrollTop: $('#messages')[0].scrollHeight});
-
-    if(event.data.match(/^[^:]* joined/)) {
-        var user = event.data.replace(/ .*/, '');
-        users.push(user);
-        refreshUsers();
-    }
-
-    if(event.data.match(/^[^:]* disconnected/)) {
-        var user = event.data.replace(/ .*/, '');
-        var idx = users.indexOf(user);
-        users = users.slice(0, idx).concat(users.slice(idx + 1));
-        refreshUsers();
-    }
 }
 
 $(document).ready(function () {
@@ -42,35 +23,12 @@ $(document).ready(function () {
         var user = $('#user').val();
         var ws = createWebSocket('/');
 
-        ws.onopen = function() {
-            ws.send('Hi! I am ' + user);
-        };
-
         ws.onmessage = function(event) {
-            if(event.data.match('^Welcome! Users: ')) {
-                /* Calculate the list of initial users */
-                var str = event.data.replace(/^Welcome! Users: /, '');
-                if(str != "") {
-                    users = str.split(", ");
-                    refreshUsers();
-                }
+			$('#join-section').hide();
+			$('#chat-section').show();
+			$('#users-section').show();
 
-                $('#join-section').hide();
-                $('#chat-section').show();
-                $('#users-section').show();
-
-                ws.onmessage = onMessage;
-
-                $('#message-form').submit(function () {
-                    var text = $('#text').val();
-                    ws.send(text);
-                    $('#text').val('');
-                    return false;
-                });
-            } else {
-                $('#warnings').append(event.data);
-                ws.close();
-            }
+			ws.onmessage = onMessage;
         };
 
         $('#join').append('Connecting...');
